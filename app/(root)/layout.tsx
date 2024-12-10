d342f149e1c9d09dcaef7f995f52ba12d2f7ec14
JScoder4005 <muk4005@gmail.com>
2024-12-10 15:13:15 +0530

    Created new layout for dashoard and created new components for Sidebar, mobileNav and header

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,22 @@
+import Header from '@/components/Header';
+import MobileNavigation from '@/components/MobileNavigation';
+import Sidebar from '@/components/Sidebar';
+import React from 'react';
+
+const layout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <main className="flex h-screen">
+      {/* Sidebar */}
+      <Sidebar />
+      <section className="flex h-full flex-1 flex-col">
+        {/* MobileNavigation */}
+        <MobileNavigation />
+        {/* header */}
+        <Header />
+        <div className="main-content">{children}</div>
+      </section>
+    </main>
+  );
+};
+
+export default layout;
---
+++ app/(root)/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="flex h-screen">
+      <h1 className="text-3xl font-bold text-brand-100">
+        StoreIt - The only storage you need
+      </h1>
+    </div>
+  );
+}
