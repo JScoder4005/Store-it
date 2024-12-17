200aba00c4c35f52befdeb2ee73308a984676533
JScoder4005 <muk4005@gmail.com>
2024-12-17 17:41:56 +0530

    Created action for geting Uploaded Files from appWrite database ... Updated Package.json

--- lib/actions/file.actions.ts
+++ lib/actions/file.actions.ts
@@ -4,9 +4,10 @@ import { UploadFileProps } from '@/types';
 import { createAdminClient } from '../appwrite';
 import { InputFile } from 'node-appwrite/file';
 import { appwriteConfig } from '../appwrite/config';
-import { ID } from 'node-appwrite';
+import { ID, Models, Query } from 'node-appwrite';
 import { constructFileUrl, getFileType, parseStringify } from '../utils';
 import { revalidatePath } from 'next/cache';
+import { getCurrentUser } from './user.actions';
 
 export const uploadFile = async ({
   file,
@@ -53,6 +54,57 @@ export const uploadFile = async ({
     return parseStringify(newFile);
   } catch (error) {
     handleError(error, 'Failed to upload file');
+  }
+};
+
+const createQueries = (
+  currentUser: Models.Document
+  // types: string[],
+  // searchText: string,
+  // sort: string,
+  // limit?: number
+) => {
+  const queries = [
+    Query.or([
+      Query.equal('owner', [currentUser.$id]),
+      Query.contains('users', [currentUser.email]),
+    ]),
+  ];
+  console.log({ queries });
+
+  // if (types.length > 0) queries.push(Query.equal('type', types));
+  // if (searchText) queries.push(Query.contains('name', searchText));
+  // if (limit) queries.push(Query.limit(limit));
+
+  // if (sort) {
+  //   const [sortBy, orderBy] = sort.split('-');
+
+  //   queries.push(
+  //     orderBy === 'asc' ? Query.orderAsc(sortBy) : Query.orderDesc(sortBy)
+  //   );
+  // }
+
+  return queries;
+};
+
+export const getFiles = async () => {
+  const { databases } = await createAdminClient();
+
+  try {
+    const currentUser = await getCurrentUser();
+
+    if (!currentUser) throw new Error('User not found');
+
+    const queries = createQueries(currentUser);
+    const files = await databases.listDocuments(
+      appwriteConfig.databaseId,
+      appwriteConfig.filesCollectionId,
+      queries
+    );
+
+    return parseStringify(files);
+  } catch (error) {
+    handleError(error, 'Failed to get files');
   }
 };
 
