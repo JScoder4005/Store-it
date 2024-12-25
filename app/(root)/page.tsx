0ffc1069cd73cb7a0c7300f8594d91b7c106a269
JScoder4005 <muk4005@gmail.com>
2024-12-25 20:11:53 +0530

    Changes in dashboard header

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div className="flex h-screen">
       <h1 className="text-3xl font-bold text-brand-100">
-        StoreIt - The only storage you need
+        StoreIt - The only storage you Need
       </h1>
     </div>
   );
