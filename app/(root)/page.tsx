01013f7654b1db4c04fd5a66ef4f60231856169d
JScoder4005 <muk4005@gmail.com>
2024-12-31 16:10:39 +0530

    Last Commit before New year 2025 :)

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div className="flex h-screen">
       <h1 className="text-3xl font-bold text-brand-100">
-        StoreIt - The only storage you Need
+        StoreIt - The only storage you need
       </h1>
     </div>
   );
